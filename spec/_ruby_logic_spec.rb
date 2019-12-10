4d59c62cb7045ee3a1957f9b057c3d77e1bdcfcf
Student <example@example.com>
2019-12-10 10:05:24 -0800

    finalize

---
+++ spec/_ruby_logic_spec.rb
@@ -0,0 +1,13 @@
+require('rspec')
+require('_ruby_logic')
+
+
+describe('#scrabble')do
+  it('should calculate the point value of a letter')do
+    expect(scrabble('A')).to(eq(1))
+  end
+  ('#scrabble')
+    it('should push all numbers from the array')do
+      expect(scrabble('A')).to(eq(1))
+    end
+end
